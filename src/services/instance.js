5ed89a6023ac95ea91e4e40cb3aa9b09ed51e720
Federico Castañares <federico.castanares@gmail.com>
2021-11-12 15:26:20 -0300

    instance

--- src/services/instance.js
+++ src/services/instance.js
@@ -22,11 +22,9 @@ axiosApiInstance.interceptors.response.use((response) => {
   return response
 }, async function (error) {
   const originalRequest = error.config;
-  console.log(originalRequest);
-  console.log(error);
   if ((error.response.status === 403 || error.response.status === 401) && !originalRequest._retry) {
     deauthenticateUser();
-    //window.location = "/login";
+    window.location = "/login";
     return;
   } else if (( error.response.status === 403 || error.response.status === 401) && !originalRequest._retry){
     console.log('no necesitaba actualizar')
@@ -34,7 +32,7 @@ axiosApiInstance.interceptors.response.use((response) => {
   else {
     console.log(error);
   }
-  //window.location = "/login";
+  window.location = "/login";
   return Promise.reject(error);
 });
 
