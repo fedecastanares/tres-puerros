a0872f26cd8b1a86b59f885f151c30d97e48c186
Federico Castañares <federico.castanares@gmail.com>
2022-01-31 16:51:07 -0300

    tamaños

--- src/components/table.js
+++ src/components/table.js
@@ -72,8 +72,8 @@ const MyTable = () => {
                 <TableHead>
                     <TableRow>
                         <StyledTableCell className={classes.item}>{title}</StyledTableCell>
-                        <StyledTableCell className={classes.smallCell}>Precio</StyledTableCell>
-                        <StyledTableCell className={classes.smallCell} >Unidades</StyledTableCell>
+                        <StyledTableCell className={classes.smallCell}>$</StyledTableCell>
+                        <StyledTableCell className={classes.smallCell} >Un</StyledTableCell>
                         <StyledTableCell className={classes.smallCell}>Kg</StyledTableCell>
                         <StyledTableCell className={classes.smallCell}>Package</StyledTableCell>
                         <StyledTableCell ></StyledTableCell>
